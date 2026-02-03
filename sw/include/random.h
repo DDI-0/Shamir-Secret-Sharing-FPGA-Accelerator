fd3031135ffa8bc270a90ff1b1eae87dff021a6b
DDI-0 <bthank445@gmail.com>
2026-02-02 20:25:23 -0500

    Create random.h

---
+++ sw/include/random.h
@@ -0,0 +1,18 @@
+/**
+ * random.h - Cryptographic random number generation
+ */
+
+#ifndef RANDOM_H
+#define RANDOM_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+
+
+int generate_random_bytes(void *buf, size_t len);
+
+
+uint64_t generate_random_mod(uint64_t max);
+
+#endif 
